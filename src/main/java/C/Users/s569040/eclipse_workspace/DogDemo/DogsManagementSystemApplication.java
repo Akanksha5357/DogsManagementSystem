08764d49ae5238940003bce7f59a6a0ba61ba6d6
Akanksha5357 <156253285+Akanksha5357@users.noreply.github.com>
2024-11-24 23:30:33 -0600

    added files

---
+++ src/main/java/C/Users/s569040/eclipse_workspace/DogDemo/DogsManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package C.Users.s569040.eclipse_workspace.DogDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogsManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogsManagementSystemApplication.class, args);
+	}
+
+}
